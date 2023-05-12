82dcb9131f9819b39057152bfdee2e0145f1b272
Tausif <tsftousif2@gmail.com>
2023-05-13 02:09:50 +0800

    Finish styling.

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "../styles/Contact.scss"
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div class="container">
+    <h1>Contact Us</h1>
+    <form>
+      <input type="text" name="name" placeholder="Your Name"/>
+      <input type="email" name="email" placeholder="Your Email"/>
+      <textarea name="message" placeholder="Your Message"></textarea>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
   )
 }
 
